b05eafbda260f738bf903dafae9518668ff7e720
me-letrongnghia <nghialee2301@gmail.com>
2025-06-03 03:15:29 +0700

    Check calander booking

--- backend/spring-boot-car-rental/src/main/java/com/example/rental/repository/BookingRepository.java
+++ backend/spring-boot-car-rental/src/main/java/com/example/rental/repository/BookingRepository.java
@@ -2,13 +2,33 @@ package com.example.rental.repository;
 
 import com.example.rental.entity.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Long> {
     List<Booking> findByRenterId(Long renterId);
     List<Booking> findByCarId(Long carId);
+
+    @Query("""
+    SELECT b FROM Booking b
+    WHERE b.car.id = :carId
+    AND b.status IN ('PENDING', 'CONFIRMED')
+    AND (
+            (:startDate BETWEEN b.startDate AND b.endDate)
+            OR (:endDate BETWEEN b.startDate AND b.endDate)
+            OR (b.startDate BETWEEN :startDate AND :endDate)
+        )
+    """)
+    List<Booking> findOverlappingBookings(
+            @Param("carId") Long carId,
+            @Param("startDate") LocalDate startDate,
+            @Param("endDate") LocalDate endDate
+    );
+
 }
 
